26e287e7d3ee2feb63411f4ea628af9a411fdf19
Darwindev <darwinlopez0308@gmail.com>
2025-12-19 22:30:45 -0500

    3-column preview card component

--- proyect-7/src/App.jsx
+++ proyect-7/src/App.jsx
@@ -11,25 +11,25 @@ function App() {
           <div className="bg-gold-400 w-full p-10">
             <img src={imgSedans} alt="" />
             <h1 className="uppercase text-white text-4xl font-bold my-8 font-big">Sedans</h1>
-            <p className="text-start font-big text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Choose a sedan for its affordanbility and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-            <button type="button" className="bg-white px-8 py-3 shadow-lg rounded-full inline-block">
-              <p className="text-base  text-gold-400 text-center font-900 font-lexend cursor-pointer">Learn More</p>
+            <p className="text-start  text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Choose a sedan for its affordanbility and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+            <button type="button" className="group bg-white px-8 py-3 shadow-lg rounded-full inline-block border-2 active:bg-transparent outline-none active:border-white">
+              <p className="text-base  text-gold-400 text-center font-900 font-lexend cursor-pointer group-active:text-white">Learn More</p>
             </button>
           </div>
           <div className="bg-cyan-800 w-full p-10">
             <img src={imgSuvs} alt="" />
             <h1 className="uppercase text-white text-4xl font-bold my-8 font-big">Suvs</h1>
-            <p className="text-start font-big text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures</p>
-            <button type="button" className="bg-white px-8 py-3 shadow-lg rounded-full inline-block">
-              <p className="text-base  text-cyan-800 text-center font-900 font-lexend cursor-pointer">Learn More</p>
+            <p className="text-start text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures</p>
+            <button type="button" className="group bg-white px-8 py-3 shadow-lg active:bg-transparent rounded-full border-2 outline-none active:border-white inline-block">
+              <p className="text-base  text-cyan-800 text-center font-900 font-lexend cursor-pointer  group-active:text-white">Learn More</p>
             </button>
           </div>
           <div className="bg-green-950 w-full p-10">
             <img src={imgLuxury} alt="" />
             <h1 className="uppercase text-white text-4xl font-bold my-8 font-big">Luxury</h1>
-            <p className="text-start font-big text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Cruise in the best card brands wothout the bloated prices. Enjoy the enhanced confort of a luxury rental and arrive in style.</p>
-            <button type="button" className="bg-white px-8 py-3 shadow-lg rounded-full inline-block">
-              <p className="text-base  text-green-950 text-center font-900 font-lexend cursor-pointer">Learn More</p>
+            <p className="text-start text-whiteTransparent mb-5 md:mb-20 font-light font-lexend">Cruise in the best card brands wothout the bloated prices. Enjoy the enhanced confort of a luxury rental and arrive in style.</p>
+            <button type="button" className="group bg-white px-8 py-3 shadow-lg rounded-full inline-block active:bg-transparent border-2 outline-none active:border-white">
+              <p className="text-base  text-green-950 text-center font-900 font-lexend cursor-pointer group-active:text-white">Learn More</p>
             </button>
           </div>
         </div>
